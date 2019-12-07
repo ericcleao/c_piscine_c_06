851ad995df7a2ef70b219403c2220128d6a05299
Eric Cerqueira Leao <ecerquei@student.42sp.org.br>
2019-12-07 00:05:58 -0300

    c_piscine_c_06 Add "ft_print_program_name" - ex00
    - Program that prints the executable name
    
    Signed-off-by: Eric Cerqueira Leao <ecerquei@student.42sp.org.br>

---
+++ ex00/ft_print_program_name.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_program_name.c                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ecerquei <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/05 21:31:54 by ecerquei          #+#    #+#             */
+/*   Updated: 2019/12/05 21:37:10 by ecerquei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str);
+
+int		main(int argc, char *argv[])
+{
+	if (argc >= 0)
+	{
+		ft_putstr(argv[0]);
+	}
+}
+
+void	ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+	write(1, "\n", 1);
+}
